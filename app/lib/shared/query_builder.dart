c25e09dea47ace2d8a9529edcd197ecc8af08cb6
noor-tg <nour.dev.00@gmail.com>
2025-08-11 19:22:32 +0300

    feat(app): add sqflite query builder
    - where, whereIn, whereBetween, whereLike, offset, limit, getAll, getOne

---
+++ app/lib/shared/query_builder.dart
@@ -0,0 +1,120 @@
+import 'package:mal/shared/db.dart';
+
+class QueryBuilder {
+  final String _table;
+  String? _sortBy;
+
+  String? _filterQuery;
+  final List<Object?> _whereArgs = [];
+
+  int _limit = 10;
+  int _offset = 0;
+
+  // getters
+  String get table => _table;
+  String? get sortBy => _sortBy;
+  String? get filterQuery => _filterQuery;
+  int get goffset => _offset;
+  int get glimit => _limit;
+  List<Object?> get whereArgs => _whereArgs;
+
+  QueryBuilder({required String table}) : _table = table;
+
+  QueryBuilder where(String column, String param, String value) {
+    _filterQuery ??= '';
+    final otherFilterExist = _filterQuery != null && _filterQuery != '';
+
+    if (column.trim().isNotEmpty && param.trim().isNotEmpty) {
+      _filterQuery =
+          '$_filterQuery ${otherFilterExist ? 'AND' : ''} $column $param ?'
+              .trim();
+      _whereArgs.add(value);
+    }
+    return this;
+  }
+
+  QueryBuilder whereLike(String column, String value) {
+    _filterQuery ??= '';
+    final otherFilterExist = _filterQuery != null && _filterQuery != '';
+
+    if (column.trim().isNotEmpty && value.trim().isNotEmpty) {
+      _filterQuery =
+          '$_filterQuery ${otherFilterExist ? 'AND' : ''} $column LIKE ?'
+              .trim();
+      // WARNING: this could cause sql injection. how to fix it ?
+      _whereArgs.add('%$value%');
+    }
+    return this;
+  }
+
+  QueryBuilder whereIn(String column, List<String> values) {
+    _filterQuery ??= '';
+    final otherFilterExist = _filterQuery != null && _filterQuery != '';
+
+    if (column.isNotEmpty && values.isNotEmpty) {
+      final placeholder = values.length > 1
+          ? values.map((_) => '?').join(', ')
+          : '?';
+      _filterQuery =
+          '$_filterQuery ${otherFilterExist ? 'AND' : ''} $column in ($placeholder)'
+              .trim();
+      _whereArgs.addAll(values);
+    }
+    return this;
+  }
+
+  QueryBuilder whereBetween<T>(String column, T min, T max) {
+    _filterQuery ??= '';
+    final otherFilterExist = _filterQuery != null && _filterQuery != '';
+
+    if (column.trim().isNotEmpty &&
+        min.toString().trim().isNotEmpty &&
+        max.toString().trim().isNotEmpty) {
+      _filterQuery =
+          '$_filterQuery ${otherFilterExist ? 'AND' : ''} $column >= ? AND $column <= ?'
+              .trim();
+      _whereArgs
+        ..add(min)
+        ..add(max);
+    }
+    return this;
+  }
+
+  Future<List<Map<String, Object?>>> getAll() async {
+    final db = await Db.use();
+    final res = await db.query(
+      _table,
+      where: _filterQuery,
+      whereArgs: _whereArgs,
+      limit: _limit,
+      offset: _offset,
+    );
+
+    return res;
+  }
+
+  Future<Map<String, Object?>> getOne() async {
+    final db = await Db.use();
+    final res = await db.query(
+      _table,
+      where: _filterQuery,
+      whereArgs: _whereArgs,
+      limit: 1,
+      offset: _offset,
+    );
+
+    return res.first;
+  }
+
+  QueryBuilder limit(int value) {
+    _limit = value;
+
+    return this;
+  }
+
+  QueryBuilder offset(int value) {
+    _offset = value;
+
+    return this;
+  }
+}
---
+++ app/lib/test/shared/query_builder_test.dart
@@ -0,0 +1,108 @@
+// ignore: depend_on_referenced_packages
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:mal/data.dart';
+import 'package:mal/shared/query_builder.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+void main() {
+  setUpAll(() async {
+    await dotenv.load();
+    databaseFactory = databaseFactoryFfi;
+    await generateData();
+  });
+  group(QueryBuilder, () {
+    test(
+      'set table on init, defaults for where , whereArgs, limit, offset',
+      () async {
+        final q = QueryBuilder(table: 'entries');
+
+        expect(QueryBuilder, isNot(null));
+
+        expect(q.table, isA<String>());
+        expect(q.table, 'entries');
+
+        expect(q.glimit, isA<int>());
+        expect(q.glimit, 10);
+
+        expect(q.goffset, isA<int>());
+        expect(q.goffset, 0);
+
+        expect(q.filterQuery, isA<String?>());
+        expect(q.filterQuery, null);
+
+        expect(q.whereArgs, isA<List<Object?>>());
+        expect(q.whereArgs, []);
+
+        expect(q.sortBy, isA<String?>());
+        expect(q.sortBy, null);
+      },
+    );
+
+    test('filter by single column', () {
+      final q = QueryBuilder(table: 'entries');
+
+      // ignore: cascade_invocations
+      q.where('category', '=', 'test');
+
+      expect(q.filterQuery, 'category = ?');
+      expect(q.whereArgs, ['test']);
+    });
+
+    test('filter by like', () {
+      final q = QueryBuilder(table: 'entries');
+
+      // ignore: cascade_invocations
+      q.whereLike('category', 'test');
+
+      expect(q.filterQuery, 'category LIKE ?');
+      expect(q.whereArgs, ['%test%']);
+    });
+
+    test('filter by in list', () {
+      final q = QueryBuilder(table: 'entries');
+
+      // ignore: cascade_invocations
+      q.whereIn('category', ['ok', 'test']);
+
+      expect(q.filterQuery, 'category in (?, ?)');
+      expect(q.whereArgs, ['ok', 'test']);
+    });
+
+    test('filter by range', () {
+      final q = QueryBuilder(table: 'entries');
+
+      // ignore: cascade_invocations
+      q.whereBetween<int>('amount', 0, 10);
+
+      expect(q.filterQuery, 'amount >= ? AND amount <= ?');
+      expect(q.whereArgs, [0, 10]);
+    });
+
+    test('when use where 2 times there is and in filterQuery', () {
+      final q = QueryBuilder(table: 'entries');
+
+      // ignore: cascade_invocations
+      q.where('category', '=', 'test').where('description', '=', 'ok');
+
+      expect(q.filterQuery, 'category = ? AND description = ?');
+      expect(q.whereArgs, ['test', 'ok']);
+    });
+
+    test('get list from table', () async {
+      final q = QueryBuilder(table: 'entries');
+
+      final result = await q.getAll();
+      expect(result, isA<List<Map<String, Object?>>>());
+      expect(result.length, greaterThan(0));
+    });
+
+    test('get single row from table', () async {
+      final q = QueryBuilder(table: 'entries');
+
+      final result = await q.getOne();
+      expect(result, isA<Map<String, Object?>>());
+      expect(result['uid'], isA<String>());
+    });
+  });
+}
