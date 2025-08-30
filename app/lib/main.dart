eedb9b072df8c4f2ac2f06d2d618c0e6d65899ec
noor-tg <nour.dev.00@gmail.com>
2025-08-30 18:26:57 +0300

    chore: cleanup

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mal/ui/mal_app.dart';
 
 void main() async {
@@ -9,5 +8,5 @@ void main() async {
 
 Future<Widget> initMalApp() async {
   await dotenv.load();
-  return const ProviderScope(child: MalApp());
+  return const MalApp();
 }
