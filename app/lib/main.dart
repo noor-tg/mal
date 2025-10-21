09bfd5dad3760de746273a83a2499c002a7bedd9
noor-tg <nour.dev.00@gmail.com>
2025-10-21 19:08:51 +0300

    docs(app): add usability study plan for version 1.2.30

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:intl/date_symbol_data_local.dart';
+// ignore: unused_import
+import 'package:mal/data.dart';
 import 'package:mal/ui/mal_app.dart';
 
 void main() async {
@@ -10,5 +12,6 @@ void main() async {
 Future<Widget> initMalApp() async {
   await initializeDateFormatting();
   await dotenv.load();
+  // await generateData();
   return const MalApp();
 }
